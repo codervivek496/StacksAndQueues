49a801d66109f525f05cf135902dec9fda827a14
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-29 09:40:47 +0530

    Added to main

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Stacks and Queues program");
+        }
+    }
+
+}
